088ff63372c3ae0977d586fbfaab94c302c4e5f3
dev-vicky-29 <150275960+dev-vicky-29@users.noreply.github.com>
2025-01-03 23:06:24 +0530

    Create Solution.java

---
+++ HackerRank/Java/Introduction/Java Stdin and Stdout I/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt(); // Read the first integer
+        int b = scan.nextInt(); // Read the second integer
+        int c = scan.nextInt(); // Read the third integer
+        scan.close(); // Close the scanner
+
+        System.out.println(a); // Print the first integer
+        System.out.println(b); // Print the second integer
+        System.out.println(c); // Print the third integer
+    }
+}
