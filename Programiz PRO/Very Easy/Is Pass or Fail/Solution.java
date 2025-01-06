7b88481b23477fef4b16fe5eb88c218164d71161
dev-vicky-29 <150275960+dev-vicky-29@users.noreply.github.com>
2025-01-06 21:15:22 +0530

    Create Solution.java

---
+++ Programiz PRO/Very Easy/Is Pass or Fail/Solution.java
@@ -0,0 +1,9 @@
+public class Solution{
+    public static String checkScore(int score) {
+         if (score >= 50) {
+            return "Pass";
+        } else {
+            return "Fail";
+        }
+    }
+}
