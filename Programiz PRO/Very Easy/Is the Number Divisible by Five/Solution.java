cf074547b077a337f9dd99380a7d584432a7231b
dev-vicky-29 <150275960+dev-vicky-29@users.noreply.github.com>
2025-01-06 21:15:25 +0530

    Create Solution.java

---
+++ Programiz PRO/Very Easy/Is the Number Divisible by Five/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static String checkDivisibility(int number){
+         if (number % 5 == 0) {
+            return "Divisible";
+        } else {
+            return "Not Divisible";
+        }
+    }
+}
