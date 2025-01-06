20a205100dfd66eb68d514487e42ecb05fd1ef3a
dev-vicky-29 <150275960+dev-vicky-29@users.noreply.github.com>
2025-01-06 21:15:10 +0530

    Create Solution.java

---
+++ Programiz PRO/Very Easy/Can Enter Club/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+
+    public static String checkAge(int age) {
+        if (age >= 18) {
+            return "Can Enter";
+        } else {
+            return "Cannot Enter";
+        }
+    }
+}
