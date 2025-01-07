17d919ad54834d95c4e9137cacdcdfd719569467
dev-vicky-29 <150275960+dev-vicky-29@users.noreply.github.com>
2025-01-07 21:36:55 +0530

    Create Solution.java

---
+++ Programiz PRO/Very Easy/Is Number Negative/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static boolean isNegative(int number) {
+        if(number < 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
